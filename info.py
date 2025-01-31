e5b885da548f52ae58b5cc9e21b97e8331905af3
Akash2025-lab <akhashalder74@gmail.com>
2025-01-31 19:52:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -86,7 +86,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/moviesearchgroup25')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/vfzteueei')
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'sunny55582636') # Support Chat Link Without https:// or @
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+ImpkR7PMd7E1NzU1') # Support Chat Link Without https:// or @
 OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/sunny55582636')
 
 # True Or False
